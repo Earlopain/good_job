f37f32385c609c099d5587fa541071f1a93f022c
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-09-13 13:47:20 +0200

    Bump RubocCop, resolve test-only 3.4 warning
    See https://bugs.ruby-lang.org/issues/20680

--- spec/lib/good_job/probe_server_spec.rb
+++ spec/lib/good_job/probe_server_spec.rb
@@ -12,8 +12,7 @@ RSpec.describe GoodJob::ProbeServer do
 
     it "responds to the expected routes" do
       get "/"
-      # See https://github.com/rspec/rspec-rails/pull/2771 for why can't use have_http_status
-      expect(last_response.status).to eq 200 # rubocop:disable RSpecRails/HaveHttpStatus
+      expect(last_response).to have_http_status(:ok)
       expect(last_response.body).to eq("OK")
 
       get "/status"
@@ -26,7 +25,7 @@ RSpec.describe GoodJob::ProbeServer do
       expect(last_response.body).to eq("Not connected")
 
       get "/unimplemented_url"
-      expect(last_response.status).to eq 404 # rubocop:disable RSpecRails/HaveHttpStatus
+      expect(last_response).to have_http_status(:not_found)
     end
   end
 
